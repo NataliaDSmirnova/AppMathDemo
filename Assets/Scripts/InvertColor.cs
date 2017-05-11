29469cbf5ca47bf8cf21a794b00108388772e47f
Kamila1625 <kamila.de.bua.tresy@gmail.com>
2017-05-11 23:03:53 +0300

    InvertColor effect

--- Assets/Scripts/InvertColor.cs
+++ Assets/Scripts/InvertColor.cs
@@ -6,79 +6,18 @@ using UnityEngine.Rendering;
 
 public class InvertColor : MonoBehaviour
 {
-    public Shader m_IndertShader;
-    private Material m_Material;
+    private Material material;
 
-    public Camera cam;
-    private Dictionary<Camera, CommandBuffer> m_Cameras = new Dictionary<Camera, CommandBuffer>();
-
-    // Use this for initialization
-    void Start()
+    // Creates a private material used to the effect
+    void Awake()
     {
-
+        material = new Material(Shader.Find("Hidden/InvertColorShader"));
     }
 
-    // Update is called once per frame
-    void Update()
+    // Postprocess the image
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-
+        Graphics.Blit(source, destination, material);
     }
 
-    // Remove command buffers from all cameras we added into
-    private void Cleanup()
-    {
-        foreach (var cam in m_Cameras)
-        {
-            if (cam.Key)
-            {
-                cam.Key.RemoveCommandBuffer(CameraEvent.AfterSkybox, cam.Value);
-            }
-        }
-        m_Cameras.Clear();
-        Object.DestroyImmediate(m_Material);
-    }
-
-    public void OnEnable()
-    {
-        Cleanup();
-    }
-
-    public void OnDisable()
-    {
-        Cleanup();
-    }
-
-    public void OnWillRenderObject()
-    {
-        CommandBuffer buf = new CommandBuffer();
-        buf.name = "Invert colors";
-
-        if (!m_Material)
-        {
-            m_Material = new Material(m_IndertShader);
-            m_Material.hideFlags = HideFlags.HideAndDontSave;
-        }
-
-        var cam = Camera.current;
-        if (!cam)
-            return;
-
-        m_Cameras[cam] = buf;
-
-        // copy screen into temporary RT
-        int screenCopyID = Shader.PropertyToID("_ScreenCopyTexture");
-        buf.GetTemporaryRT(screenCopyID, -1, -1, 0, FilterMode.Bilinear);
-        buf.Blit(BuiltinRenderTextureType.CurrentActive, screenCopyID);
-
-        int invertID = Shader.PropertyToID("_Temp1");
-        buf.GetTemporaryRT(invertID, -1, -1, 0, FilterMode.Bilinear);
-
-        buf.Blit(screenCopyID, invertID);
-        buf.ReleaseTemporaryRT(screenCopyID);
-        buf.Blit(screenCopyID, invertID, m_Material);
-
-        buf.SetGlobalTexture("_InvertColorTexture", invertID);
-
-        cam.AddCommandBuffer(CameraEvent.AfterEverything, buf);
-    }
 }
---
+++ Assets/Shaders/InvertColorShader.shader
@@ -0,0 +1,28 @@
+﻿Shader "Hidden/InvertColorShader"
+{
+	Properties
+	{
+		_MainTex("Base (RGB)", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			uniform sampler2D _MainTex;
+
+			float4 frag(v2f_img i) : COLOR
+			{
+				float4 c = 1 - tex2D(_MainTex, i.uv);
+				return c;
+			}
+
+			ENDCG
+		}
+	}
+}
